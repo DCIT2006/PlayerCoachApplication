9788b0982e6a82f9160a35c2454309e637e86205
Derek Moore <mooreit2006@gmail.com>
2025-10-02 18:32:29 -0400

    Working

--- PlayerCoachApplication.Data/Migrations/20250909225528_AddSportToCoachApplicationViewModel.Designer.cs
+++ PlayerCoachApplication.Data/Migrations/20250909225528_AddSportToCoachApplicationViewModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlayerCoachApplication.Data.Context;
 
@@ -11,9 +12,11 @@ using PlayerCoachApplication.Data.Context;
 namespace PlayerCoachApplication.Data.Migrations
 {
     [DbContext(typeof(PlayerApplicationDBContext))]
-    partial class PlayerApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250909225528_AddSportToCoachApplicationViewModel")]
+    partial class AddSportToCoachApplicationViewModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PlayerCoachApplication.Data/Migrations/20250909225528_AddSportToCoachApplicationViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlayerCoachApplication.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSportToCoachApplicationViewModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Sport",
+                table: "CoachApplicationModel",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sport",
+                table: "CoachApplicationModel");
+        }
+    }
+}
